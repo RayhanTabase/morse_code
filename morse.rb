4dd8aacca707df4515028143018893d015074d42
salimabdulai <maouslayer@gmail.com>
2022-02-24 08:59:41 +0000

    Add solution

--- morse.rb
+++ morse.rb
@@ -1,48 +1,49 @@
 MORSE_KEYS = {
-  '.-'=> 'A',
-  '-...'=> 'B',
-  '-.-.'=> 'C',
-  '-..'=> 'D',
-  '.'=> 'E',
-  '..-.'=> 'F',
-  '--.'=> 'G',
-  '....'=> 'H',
-  '..'=> 'I',
-  '-.-'=> 'K',
-  '.-..'=> 'L',
-  '--'=> 'M',
-  '-.'=> 'N',
-  '---'=> 'O',
-  '.--.'=> 'P',
-  '--.-'=> 'Q',
-  '.-.'=> 'R',
-  '...'=> 'S',
-  '-'=> 'T',
-  '..-'=> 'U',
-  '...-'=> 'V',
-  '.--'=> 'W',
-  '-..-'=> 'X',
-  '-.--'=> 'Y',
-  '--..'=> 'Z'
-}
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
+}.freeze
 
 def convert_code(code)
-  return MORSE_KEYS[code]
+  MORSE_KEYS[code]
 end
 
-
 def decode_message(message)
-  words = message.split(' ')
+  words = message.split(/\s/)
   puts words
   decoded = ''
   words.each do |word|
     if word.include? '.' or word.include? '-'
-      decoded = decoded + convert_code(word)
+      decoded += convert_code(word)
     else
-      decoded = decoded + ' ' + word + ' '
+      puts word
+      decoded += ' ' if word == ''
+      decoded += word
     end
-    puts "#{decoded}"
   end
+  puts decoded
 end
 
 decode_message '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-. / .-. ..- -... .. . ...'
